bd585bb32f06cbb3c60578c6dbcdf48a18283099
Tarik Ponciano <tarikponciano@gmail.com>
2025-02-06 09:53:08 -0300

    Conexão criada

--- game.gd
+++ game.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+# 1. Criar interface com botões
+# Host e Join.
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# 2. Criar a infraestrutura de
+# conexão desses botões. 
+# Código de Join Server
+# Código de Create Server
 
+# 3. Exibir informação na tela ou
+# terminal, quando um usuário se 
+# conectar.
+var peer = ENetMultiplayerPeer.new()
+const ADDRESS = "127.0.0.1"
+const PORT = 3333
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Rodar o create_client
+# Alterar o multiplayer godot para ser o peer
+func _on_botao_join_pressed() -> void:
+	peer.create_client(ADDRESS,PORT)
+	multiplayer.multiplayer_peer = peer
+	
+# Rodar o create_server
+# Alterar o multiplayer godot para ser o peer
+func _on_botao_host_pressed() -> void:
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
