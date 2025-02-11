a132484f6119533fcc9853b710226156931674ec
Tarik Ponciano <everton-zero@hotmail.com>
2025-02-11 11:25:16 -0300

    Camera
    Add camera player

---
+++ player.gd
@@ -0,0 +1,46 @@
+#extends CharacterBody2D
+#
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Quando o nó é carregado, garantimos que o cliente tem autoridade sobre o jogador
+#func _ready() -> void:
+	#if is_multiplayer_authority():
+		## Define a autoridade corretamente para o jogador
+		#set_multiplayer_authority(multiplayer.get_unique_id())  # Atribui autoridade ao cliente correto
+#
+#func _physics_process(delta: float) -> void:
+	## Só permite o movimento do personagem se o cliente tiver autoridade sobre ele
+	#if is_multiplayer_authority():
+		#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		#velocity = direction * SPEED
+		#move_and_slide()  # Aplica o movimento
+extends CharacterBody2D
+
+const SPEED = 300.0
+@onready var rotulo_nome = $NomeJogador
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+	
+	if is_multiplayer_authority(): #Verifica se autoridade 
+		var camera = Camera2D.new()
+		add_child(camera)
+	
+func _physics_process(delta: float) -> void:
+	if (is_multiplayer_authority()):
+		
+		var direction_horizontal := Input.get_axis("ui_left", "ui_right")
+		if direction_horizontal:
+			velocity.x = direction_horizontal * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+		var direction_vertical := Input.get_axis("ui_up", "ui_down")
+		if direction_vertical:
+			velocity.y = direction_vertical * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
