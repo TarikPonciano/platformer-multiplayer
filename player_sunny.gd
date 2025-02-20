41b2ca5b75fc1782165506432f70f7e3363a03ac
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-20 08:51:04 -0300

    Ultima versao

--- player_sunny.gd
+++ player_sunny.gd
@@ -41,12 +41,17 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 func _update_animation() -> void:
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+		
+	
 	if velocity.y < 0:
 		$AnimatedSprite2D.play("jump")
 	elif velocity.y > 0:
 		$AnimatedSprite2D.play("fall")
 	elif velocity.x != 0:
 		$AnimatedSprite2D.play("run")
-		$AnimatedSprite2D.flip_h = velocity.x < 0  # Inverte o sprite com base na direção
 	else:
 		$AnimatedSprite2D.play("idle")
