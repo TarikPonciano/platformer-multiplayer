cdf8a5b72767de633bdb319c11d3ec71854d35ee
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-06 11:20:15 -0300

    Update bola_de_fogo.gd

--- bola_de_fogo.gd
+++ bola_de_fogo.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @onready var animation = $AnimatedSprite2D
-const SPEED = 300
+const SPEED = 100
 var direction = 1 # Variável que define a direção do movimento e da animação
 @export var projectileOwner:String # Define o dono da bala
 
