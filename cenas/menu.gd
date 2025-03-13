f200c610897537a46bc564b3670c47eda22fa115
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-13 09:53:27 -0300

    Menu implementado

--- cenas/menu.gd
+++ cenas/menu.gd
@@ -22,7 +22,14 @@ func _on_botao_foxy_pressed() -> void:
 	botaoFoxy.add_theme_color_override("font_color", Color(255,255,0))
 	botaoFoxy.add_theme_color_override("font_hover_color",Color(255,255,0))
 	botaoFoxy.add_theme_color_override("font_focus_color",Color(255,255,0))
+	
+	if (botaoSunny.has_theme_color_override("font_color")):
+		botaoSunny.remove_theme_color_override("font_hover_color")
+		botaoSunny.remove_theme_color_override("font_focus_color")
+		botaoSunny.remove_theme_color_override("font_color")
+	
 	animacaoFoxy.play("run")
+	animacaoSunny.play("idle")
 
 
 func _on_botao_sunny_mouse_entered() -> void:
@@ -40,4 +47,9 @@ func _on_botao_sunny_pressed() -> void:
 	botaoSunny.add_theme_color_override("font_color", Color(255,255,0))
 	botaoSunny.add_theme_color_override("font_hover_color",Color(255,255,0))
 	botaoSunny.add_theme_color_override("font_focus_color",Color(255,255,0))
+	if (botaoFoxy.has_theme_color_override("font_color")):
+		botaoFoxy.remove_theme_color_override("font_hover_color")
+		botaoFoxy.remove_theme_color_override("font_focus_color")
+		botaoFoxy.remove_theme_color_override("font_color")
 	animacaoSunny.play("run")
+	animacaoFoxy.play("idle")
